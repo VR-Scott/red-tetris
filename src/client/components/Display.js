00a468b66edecf2ce5b5e93de8d67ae41912f174
Salsa-socks <dbongs@gmail.com>
2020-09-09 18:31:19 +0200

    semantic ui for typography and header

--- src/client/components/Display.js
+++ src/client/components/Display.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StyledDisplay } from './styles/StyledDisplay';
-import { Typography } from "@material-ui/core";
+import { Header } from 'semantic-ui-react';
 
 const Display = ({ gameOver, text, id }) => {
   return (<StyledDisplay gameOver={gameOver} id={id}>
-            <Typography>{text}</Typography>
+            <Header>{text}</Header>
           </StyledDisplay>);
 }
 
--- src/client/components/StartButton.js
+++ src/client/components/StartButton.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button, Typography } from "@material-ui/core";
-
-const StyledStartButton = styled.button`
-  box-sizing: border-box;
-
-  margin: 0 0 20px 0;
-  padding: 20px;
-  min-height: 30px;
-  width: 100%;
-  border-radius: 20px;
-  border: none;
-  color: white;
-  background: #333;
-  font-family: Pixel, Arial, Helvetica, sans-serif;
-  font-size: 1rem;
-  outline: none;
-  cursor: pointer;
-`;
+import { Button } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 
 const StartButton = ({ callback, mainSocket, setStart, newGame}) => (
   <Button variant="contained"
 id="startButton"
   onClick={() => callback(mainSocket, setStart, newGame, setStart)} fullWidth>
 
-  <Typography>START GAME</Typography>
+  <Header>START GAME</Header>
 </Button>
 );
 
