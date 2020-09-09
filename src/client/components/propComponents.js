9748cc7a64c38f7f41a45c99dcea218efb3016ff
Salsa-socks <dbongs@gmail.com>
2020-09-09 13:55:09 +0200

    changed colors

--- src/client/components/propComponents.js
+++
@@ -1,5 +0,0 @@
-import {StyledDisplay} from './styles/StyledDisplay';
-import  {StyledCell} from './styles/StyledCell';
-import {StyledStage, StyledEnemyStage} from './styles/StyledStage';
-
-export {StyledDisplay, StyledCell, StyledStage, StyledEnemyStage};
